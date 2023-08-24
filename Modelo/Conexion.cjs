7de11182158fd09f4a9f8941cc3a41bd264c65b9
Hugo Couce <hugocoucelorenzo@gmail.com>
2023-08-24 17:59:35 +0200

    Operaciones básicas con la base de datos y otras pruebas

--- Modelo/Conexion.cjs
+++ Modelo/Conexion.cjs
@@ -1,18 +1,36 @@
-export default class Conexion {
-    static mysql = require("mysql");
+mysql = require("mysql");
+class Conexion {
     static conexion = mysql.createConnection({
         host: "localhost",
         database: "granjas",
         user: "root",
         password: ""
     });
-
-    static getConexion() {
-        conexion.connect(function(err) {
-            if (err) throw err;
-            console.log("Connected!");
+    static conectar() {
+        return new Promise((resolve, reject) => {
+            this.conexion.connect((err) => {
+                if (err) {
+                    console.error('Error al conectar a la base de datos:', err);
+                    reject(err);
+                    return;
+                }
+                console.log('Conexión exitosa a la base de datos.');
+                resolve();
+            });
         });
-        return this.conexion;
+    }
+    static con() {
+        return new Promise((resolve, reject) => {
+            this.conexion.connect((error) => {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log("Conexion hecha");
+                }
+            })
+        });
     }
 }
 
+module.exports = Conexion;
+Conexion.con();
--- Modelo/Pruebas.cjs
+++ Modelo/Pruebas.cjs
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
-var conexion = mysql.createConnection({
+var datosConexion = mysql.createConnection({
     host: "localhost",
     database: "granjas",
     user: "root",
     password: ""
 });
-
+/*
 function getConexion() {
     conexion.connect(function(err) {
         if (err) throw err;
@@ -15,7 +15,10 @@ function getConexion() {
 }
 
 function getMaquinas() {
-    let conexion = getConexion();
+    let conexion = datosConexion.connect(function(err) {
+        if (err) throw err;
+        console.log("Conectado");
+    })
     console.log(conexion);
     let maquinas = conexion.query("SELECT * FROM maquina", function (err, result, fields) {
         if (err) throw err;
@@ -37,4 +40,70 @@ function getMaquinas() {
 
   }
 
-  getMaquinas();
+  //getMaquinas();
+  */
+ /*
+  const mysql = require('mysql');
+
+  // Configuración de la conexión a la base de datos
+  const connection = mysql.createConnection({
+    host: 'localhost', // Cambia esto a la dirección de tu servidor MySQL
+    user: 'root',   // Cambia esto al usuario de tu base de datos
+    password: '', // Cambia esto a la contraseña de tu usuario
+    database: 'granjas'  // Cambia esto al nombre de tu base de datos
+  });*/
+  
+  // Conectar a la base de datos
+
+
+
+function conectar() {
+    return new Promise((resolve, reject) => {
+        datosConexion.connect((err) => {
+            if (err) {
+                console.error('Error al conectar a la base de datos:', err);
+                reject(err);
+                return;
+            }
+            console.log('Conexión exitosa a la base de datos.');
+            resolve();
+        });
+    });
+}
+
+function getConexion() {
+    return new Promise((showMensajeConexionExitosa, showMensajeConexionFallida) => {
+        datosConexion.connect((error) => {
+            if (error) {
+                showMensajeConexionFallida("Fallo conectando");
+            } else {
+                showMensajeConexionExitosa("Conexión exitosa");
+            }
+        });
+    });
+}
+
+function getPromiseMaquinas() {
+    return new Promise((resolve, reject) => {
+        datosConexion.query("SELECT * FROM maquina", (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(result);
+        });
+    });
+}
+
+async function getMaquinas() {
+    try {
+        await getConexion();
+        const maquinas = await getPromiseMaquinas();
+        console.log('Maquinas:', maquinas);
+        datosConexion.end();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+getMaquinas();
